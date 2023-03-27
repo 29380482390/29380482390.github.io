5f86e12872b3845d0d13333bf9f19dcff7e4612b
29380482390 <51510485+29380482390@users.noreply.github.com>
2023-03-27 20:59:10 +0200

    mentally ill
    it's the same, but different according to some browsers

--- js/tablogic.js
+++ js/tablogic.js
@@ -1,27 +1,27 @@
-$(document).ready(function () {
-  $('.tabs-item.item-1').addClass('active');
-  $('.tab-header-item').first().addClass('active');
+$(document).ready(function() {
+  $('.tab.1').addClass('active');
+  $('.tabs-item').first().addClass('active');
 
-  $('.tab-header-item').click((ev) => {
-    $('.tab-header-item.active').removeClass('active');
-    $(ev.target).addClass('active');
-    linkTabs();
+  $('.tabs-item').click((ev) => {
+      $('.tabs-item.active').removeClass('active');
+      $(ev.target).addClass('active');
+      linkTabs();
   });
 
   $(document).on('keydown', (ev) => {
-    if (ev.which === 9) { 
-      ev.preventDefault(); 
-      ev.stopPropagation();
-      const $next = $('.tab-header-item.active').removeClass('active').next('.tab-header-item');
-      $next.length ? $next.addClass('active') : $(".tab-header-item:first").addClass('active');
-      linkTabs();
-    } 
+      if (ev.which === 9) {
+          ev.preventDefault();
+          ev.stopPropagation();
+          const $next = $('.tabs-item.active').removeClass('active').next('.tabs-item');
+          $next.length ? $next.addClass('active') : $(".tabs-item:first").addClass('active');
+          linkTabs();
+      }
   });
 
 });
 
 const linkTabs = () => {
-  const activeTabHeader = $('.tab-header-item.active').attr('class').split(' ')[1];
-  $('.tabs-item.active').removeClass('active');
-  $(`.tabs-item.${activeTabHeader}`).addClass('active');
+  const activeTabHeader = $('.tabs-item.active').attr('class').split(' ')[1];
+  $('.tab.active').removeClass('active');
+  $(`.tab.${activeTabHeader}`).addClass('active');
 };
