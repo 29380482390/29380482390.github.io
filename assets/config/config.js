759b2d8e9d6a42be8cc9addb1dbe2ff755240a7a
eric <51510485+29380482390@users.noreply.github.com>
2024-01-14 19:49:53 +0100

    2024
    version 1

---
+++ assets/config/config.js
@@ -0,0 +1,87 @@
+const CONFIG = {
+	openInNewTab: true,
+	twelveHourFormat: false,
+
+	greetingMorning: 'good morning.',
+	greetingAfternoon: 'good afternoon.',
+	greetingEvening: 'good evening.',
+	greetingNight: 'go to sleep.',
+
+	firstButtonsContainer: [
+		{
+			id: '1',
+			name: 'reddit',
+			icon: 'glasses',
+			link: 'https://www.reddit.com/',
+		},
+		{
+			id: '2',
+			name: 'twitter',
+			icon: 'twitter',
+			link: 'https://twitter.com/home',
+		},
+		{
+			id: '3',
+			name: 'youtube',
+			icon: 'youtube',
+			link: 'https://www.youtube.com/',
+		},
+		{
+			id: '4',
+			name: 'twitch',
+			icon: 'twitch',
+			link: 'https://www.twitch.tv/',
+		},
+		{
+			id: '5',
+			name: 'movies',
+			icon: 'film',
+			link: 'https://bflixz.to/',
+		},
+		{
+			id: '6',
+			name: 'anime',
+			icon: 'cat',
+			link: 'https://lite.aniwave.to/ongoing',
+		},
+	],
+
+	secondButtonsContainer: [
+		{
+			id: '1',
+			name: 'gmail',
+			icon: 'mail',
+			link: 'https://mail.google.com/',
+		},
+		{
+			id: '2',
+			name: 'outlook',
+			icon: 'mails',
+			link: 'https://outlook.office365.com/mail/',
+		},
+		{
+			id: '3',
+			name: 'fitgirl-repacks',
+			icon: 'sailboat',
+			link: 'https://fitgirl-repacks.site/',
+		},
+		{
+			id: '4',
+			name: 'cs.rin.ru',
+			icon: 'gamepad-2',
+			link: 'https://cs.rin.ru/forum/',
+		},
+		{
+			id: '5',
+			name: 'github',
+			icon: 'github',
+			link: 'https://github.com/',
+		},
+		{
+			id: '6',
+			name: '4chan',
+			icon: 'clover',
+			link: 'https://www.4chan.org/',
+		},
+	]
+};
